42fa7d946ccf722dccec0bd61e121e9f5792f787
Piotr Szamocki <szamocki.piotr04@gmail.com>
2023-05-05 20:39:23 +0200

    Added LoginRequiredPanel and methods to show login and register form.

--- MyMovieList/MainForm.cs
+++ MyMovieList/MainForm.cs
@@ -12,6 +12,20 @@ namespace MyMovieList
 {
     public partial class MainForm : Form
     {
+        // Set variables for user
+        public string m_username;
+        public string m_password;
+
+        public bool m_isLoggedIn = false;
+
+        // Set variables for panels
+        public LoginPanel m_loginPanel = new LoginPanel();
+        public RegisterPanel m_registerPanel = new RegisterPanel();
+        public MovieDetailPanel m_movieDetailPanel = new MovieDetailPanel();
+        public MovieSearchPanel m_movieSearchPanel = new MovieSearchPanel();
+        public UserAccountPanel m_userAccountPanel = new UserAccountPanel();
+        public UserMovieListPanel m_userMovieListPanel = new UserMovieListPanel();
+
         public MainForm()
         {
             InitializeComponent();
@@ -19,7 +33,31 @@ namespace MyMovieList
 
         private void MainForm_Load(object sender, EventArgs e)
         {
+            // Clear the form
+            Controls.Clear();
 
+            // Show the login panel
+            ShowLoginForm();
+        }
+
+        public void ShowLoginForm()
+        {
+            // Logout
+            m_isLoggedIn = false;
+
+            // Show login panel
+            Controls.Clear();
+            Controls.Add(m_loginPanel);
+        }
+
+        public void ShowRegisterForm()
+        {
+            // Logout
+            m_isLoggedIn = false;
+
+            // Show register panel
+            Controls.Clear();
+            Controls.Add(m_registerPanel);
         }
     }
 }
